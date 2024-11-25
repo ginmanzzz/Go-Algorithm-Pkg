1e940b706f226fc53b7d464d83f94f94ef7fe931
ginmanzzz <595348563@qq.com>
2024-11-25 16:02:15 +0800

    Completed pure dijkstra

---
+++ pure_dijkstra.go
@@ -0,0 +1,69 @@
+package algo
+
+import (
+	"math"
+)
+
+const inf = math.MaxInt / 2
+
+func buildGraph(edges [][3]int, n int, isDirect bool) [][]int {
+	g := make([][]int, n)
+	for i := range g {
+		g[i] = make([]int, n)
+		for j := range g[i] {
+			g[i][j] = inf
+		}
+	}
+	for _, edge := range edges {
+		x := edge[0]
+		y := edge[1]
+		w := edge[2]
+		g[x][y] = w
+		if !isDirect {
+			g[y][x] = w
+		}
+	}
+	return g
+}
+
+/*
+  @params: 	g is the builed graph(directed graph or undirected graph).
+  			k is the origin point.
+  @brief:  	Pure dijkstra algorithm is used to find the shortest way between two points, or one point to others.
+  			It can be used in positive weight graph and dense graph.
+  @returns: distance is all distances start from the origin point to others.
+  			can is whether the origin point can arrive at all others.
+  @time:   	O(N^2)
+  @space:  	O(N^2)
+*/
+func PureDijkstra(g [][] int, k int) ([]int,bool) {
+	n := len(g)
+	distance := make([]int, n)
+	for i := range distance {
+		distance[i] = inf
+	}
+	distance[k] = 0
+	can := false
+	done := make([]bool, n)
+	for {
+		x := -1
+		for i, ok := range done {
+			if !ok && (x < 0 || distance[i] < distance[x]) {
+				x = i
+			}
+		}
+		if x < 0 {
+			can = true
+			break
+		}
+		if distance[x] >= inf {
+			can = false
+			break
+		}
+		done[x] = true
+		for y, d := range g[x] {
+			distance[y] = MinInt(distance[y], distance[x] + d)
+		}
+	}
+	return distance, can
+}
